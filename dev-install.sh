763317d6352b6170ad9edc3d738f38e84892b2d0
ilseob lee <lysk9884@gmail.com>
2026-02-17 10:20:41 +0900

    refactor: simplify run_command function by removing unnecessary checks and variables

--- dev-install.sh
+++ dev-install.sh
@@ -7,8 +7,6 @@ set -o pipefail
 run_command() {
     local cmd="$1"
     local is_windows_shell=0
-    local exit_code=0
-    local cmd_path=""
     shift
 
     case "$(uname -s 2>/dev/null || true)" in
@@ -17,55 +15,18 @@ run_command() {
             ;;
     esac
 
-    if command -v "$cmd" >/dev/null 2>&1; then
-        cmd_path="$(command -v "$cmd")"
-        case "$cmd_path" in
-            [A-Za-z]:/*)
-                is_windows_shell=1
-                ;;
-        esac
-    fi
-
     if [ "$is_windows_shell" -eq 1 ] && command -v "${cmd}.cmd" >/dev/null 2>&1; then
         "${cmd}.cmd" "$@"
         return
     fi
 
-    if [ "$is_windows_shell" -eq 1 ] && [ -n "$cmd_path" ]; then
-        if "$cmd_path" "$@"; then
-            return
-        fi
-
-        exit_code=$?
-        if [ "$exit_code" -ne 127 ] && [ "$exit_code" -ne 126 ]; then
-            exit "$exit_code"
-        fi
-    fi
-
     if command -v "$cmd" >/dev/null 2>&1; then
-        if [ "$is_windows_shell" -eq 1 ] && [ -n "$cmd_path" ]; then
-            :
-        elif [ -n "$cmd_path" ]; then
-            if "$cmd_path" "$@"; then
-                return
-            fi
-
-            exit_code=$?
-            if [ "$exit_code" -ne 127 ] && [ "$exit_code" -ne 126 ]; then
-                exit "$exit_code"
-            fi
-        elif "$cmd" "$@"; then
-            return
-        else
-            exit_code=$?
-            if [ "$exit_code" -ne 127 ] && [ "$exit_code" -ne 126 ]; then
-                exit "$exit_code"
-            fi
-        fi
+        "$cmd" "$@"
+        return
     fi
 
-    if command -v "${cmd}.cmd" >/dev/null 2>&1; then
-        "${cmd}.cmd" "$@"
+    if command -v "${cmd}.exe" >/dev/null 2>&1; then
+        "${cmd}.exe" "$@"
         return
     fi
 
